d9d77bdce4bd7614198e2ac84b9bd12c4bcd8598
A Sullivan <asullivan@As-MacBook-Pro.local>
2024-11-29 21:18:23 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<button id="myButton">Click Me!</button>
+
+<script>
+    document.getElementById('myButton').addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
+</script>
+
